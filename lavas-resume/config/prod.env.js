45d50f416e2e1f3f1a984863d7d26a644c2bb1a9
王阳 <wy23623@ly.com>
2017-09-11 17:39:44 +0800

    删除多余的.

--- lavas-resume/config/prod.env.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file 生产环境相关配置文件
- * @author 王阳(wy23623@ly.com)
- */
-
-module.exports = {
-    NODE_ENV: '"production"'
-};
--- lavas-resume/src/event-bus.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file 事件总线
- * @author 王阳(wy23623@ly.com)
- */
-
-import Vue from 'vue';
-
-// 全局事件总线
-export default new Vue();
