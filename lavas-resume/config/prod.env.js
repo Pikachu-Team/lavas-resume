c92191bd42d7025f7d987cb30a7cb42a29a44687
王阳 <wy23623@ly.com>
2017-09-11 17:33:05 +0800

    1111

---
+++ lavas-resume/config/prod.env.js
@@ -0,0 +1,8 @@
+/**
+ * @file 生产环境相关配置文件
+ * @author 王阳(wy23623@ly.com)
+ */
+
+module.exports = {
+    NODE_ENV: '"production"'
+};
---
+++ lavas-resume/src/entry-skeleton.js
@@ -0,0 +1,14 @@
+/**
+ * @file 页面骨架
+ * @author 王阳(wy23623@ly.com)
+ */
+
+import Vue from 'vue';
+import Skeleton from '@/pages/Skeleton';
+
+export default new Vue({
+    components: {
+        Skeleton
+    },
+    template: '<skeleton />'
+});
